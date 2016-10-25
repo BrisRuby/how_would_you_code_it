5130521864c99f3cab19c42d1ed9c9f63afc6717
Dave Kinkead <dave@kinkead.com.au>
2016-10-25 17:54:37 +1000

    Initial commit

---
+++ crypto.rb
@@ -0,0 +1,27 @@
+# Implement methods for encrypting and decrypting a square code.
+#
+# Hint:
+#
+# "If man was meant to stay on the ground, God would have given us roots."
+# 
+# is normalised to...
+# 
+# "ifmanwasmeanttostayonthegroundgodwouldhavegivenusroots"
+# 
+# then converted to a square where columns >= rows and columns - rows <= 1
+# 
+# ifmanwas
+# meanttos
+# tayonthe
+# groundgo
+# dwouldha
+# vegivenu
+# sroots
+#
+# which when read top-down left to right gives...
+# 
+# "imtgdvsfearwermayoogoanouuiontnnlvtwttddesaohghnsseoau"
+# 
+# then spaced into row length tokens gives...
+# 
+# "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau"# 
